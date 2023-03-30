9574b5eacf25e982509cdd7dc3adb9439c92fdd5
Velorei <valeriie.alexandrovna@gmail.com>
2023-03-30 20:47:54 +0300

    let_var_lesson1.js

---
+++ let_var_lesson1.js
@@ -0,0 +1,48 @@
+var num1 = 1;
+var num2 = 12;
+
+
+// console.log(num1, num2)
+
+var name = "Vadim";
+var bb = true;
+var ff = false;
+var fl = 3.14;
+var arr = [3.14, false, 'Elena', 33, [22, 22]]
+var jj = {
+            "key1": 11,
+            "key2": 33};
+
+console.log(num2, typeof(num2))
+console.log(name33, typeof(name33))
+console.log(bb, typeof(bb))
+console.log(ff, typeof(ff))
+console.log(fl, typeof(fl))
+console.log(arr, typeof(arr))
+console.log(jj, typeof (jj))
+
+
+//----------------------------
+var num1 = 11;
+var num2 = 12;
+
+var result1 = num1 >= num2;
+var result2 = num1 <= num2;
+var result3 = num1 == num2;
+
+// console.log('result = ', result)
+
+if (result1){
+    console.log(22)
+} else if (result2) {
+    let a1 = 20;
+    let a2 = 5;
+
+    let a3 = a1 + a2
+
+    console.log(a3, 44)
+} else if (result3) {
+    console.log(66)
+} else {
+    console.log("No result")
+}
